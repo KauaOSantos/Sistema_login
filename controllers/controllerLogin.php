f81b6e4b2bd9a7ed20b54070272d79c105e01718
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2024-11-19 00:40:40 -0300

    fiz pequenos ajustes mas não consegui terminar 100%

--- controllers/controllerLogin.php
+++ controllers/controllerLogin.php
@@ -13,9 +13,7 @@ class ControllerLogin {
         }
     }
 
-    // Valida o login e redireciona para a index
     public function login($email, $senha) {
-        // Busca o usuário no banco de dados
         $consulta = "SELECT * FROM usuario WHERE email = ? LIMIT 1";
         $stmt = $this->conexao->prepare($consulta);
         $stmt->bind_param("s", $email);
@@ -23,37 +21,25 @@ class ControllerLogin {
         $resultado = $stmt->get_result();
 
         if ($resultado->num_rows == 0) {
-            // Usuário não encontrado, redireciona para a página de cadastro
             $_SESSION['erro_login'] = "Usuário não encontrado. Faça seu cadastro.";
             header("Location: ../views/cadastro_usuario.php");
             $stmt->close();
             exit();
         }
 
-        // Obtém os dados do usuário
         $usuario = $resultado->fetch_assoc();
         $stmt->close();
 
-        // Verifica a senha
         if (password_verify($senha, $usuario['senha'])) {
-            // Inicia a sessão e redireciona para a home
             $_SESSION['usuario_email'] = $usuario['email'];
             $_SESSION['usuario_id'] = $usuario['id_usuario'];
             header("Location: ../views/index.php");
             exit();
-        } else {
-            // Senha incorreta, redireciona para a página de login
+        } 
+        else {
             $_SESSION['erro_login'] = "Senha ou email incorretos!";
             header("Location: ../views/index.php");
             exit();
         }
-    }
-
-    // Função para deslogar
-    public function logout() {
-        session_unset();
-        session_destroy();
-        header("Location: ../views/index.php");
-        exit();
     }
 }
